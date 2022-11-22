23bd64bf26dda3f6984a7ef05c0c2a93bd6eb4dd
David McDonagh <david@leapfrogdbs.co.uk>
2022-11-22 09:56:17 +0000

    Convert Kit

--- template-parts/lesson-weeks/content.php
+++ template-parts/lesson-weeks/content.php
@@ -1,17 +1,24 @@
-<?php 
-$lw_content = get_field('lw_content');
 
-if ($lw_content) :
+
+
+<?php
+
+if( have_rows('week') ):
+
+    while( have_rows('week') ) : the_row();
+        $lw_content = get_sub_field('lw_content');
 ?>
 
-<section class="bg-white">
-    <div class="container">
-        <div class="row">
-            <div class="col entry-content block-editor-content md:col-span-8 md:col-start-3">
-                <?php echo $lw_content; ?>
+        <section class="bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col entry-content block-editor-content md:col-span-8 md:col-start-3">
+                        <?php echo $lw_content; ?>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
+        </section>
 
-<?php endif; ?>
+<?php 
+endwhile;
+endif; ?>
--- template-parts/lesson-weeks/header.php
+++ template-parts/lesson-weeks/header.php
@@ -1,8 +1,8 @@
 <?php
-$lw_title = get_field('lw_title');
+$lw_page_title = get_field('lw_page_title');
 $lw_subtitle = get_field('lw_subtitle');
 
-if ($lw_title) :
+if ($lw_page_title) :
 ?>
 
 
@@ -11,7 +11,7 @@ if ($lw_title) :
         <div class="row">
             <div class="col">
                 <h1 class="headingThree mb-2 text-white text-center">
-                    <?php echo $lw_title; ?>
+                    <?php echo $lw_page_title; ?>
                 </h1>
                 <?php if ($lw_subtitle) : ?>
                     <p class="headingSix text-white text-center"><?php echo $lw_subtitle; ?></p>
