4305c86b10fec7bf6719c4460117da5dd29f8f5a
David McDonagh <david@leapfrogdbs.co.uk>
2022-12-05 17:09:28 +0000

    Updates

--- functions.php
+++ functions.php
@@ -631,3 +631,15 @@ function add_img_size_attr($content){
   }
   add_filter('the_content','add_img_size_attr');
 
+  /*Defer css*/
+function add_rel_preload($html, $handle, $href, $media) {
+
+if (is_admin())
+	return $html;
+
+	$html = <<<EOT
+<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
+EOT;
+    return $html;
+}
+add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
