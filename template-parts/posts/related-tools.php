686c03b32981109af28a130b3f911832693ea3d9
David McDonagh <david@leapfrogdbs.co.uk>
2022-12-08 14:26:46 +0000

    Updates

--- template-parts/posts/related-tools.php
+++ template-parts/posts/related-tools.php
@@ -4,7 +4,6 @@
         <div class="row">
             <div class="col col-span-12 md:col-span-8 md:col-start-3 text-center">
                 <h2 class="headingTwo">Related Tools</h2>
-                <p class="bodyOne">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi placeat nisi laboriosam ut quasi blanditiis quos? Fuga tempore debitis quaerat.</p>
             </div>
         </div>
         <div class="row">
