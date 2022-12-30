1be7b013b5e084cdff06cc57d2041a96fdbcc779
David McDonagh <david@leapfrogdbs.co.uk>
2022-12-30 16:57:36 +0000

    Updates

--- template-parts/category/header.php
+++ template-parts/category/header.php
@@ -1,5 +1,12 @@
-<section class="bg-turquoise">
+<section class="bg-turquoise pt-0">
     <div class="container">
+        <div class="row ">
+            <div class="col flex flex-row gap-x-12 md:items-start justify-between mb-6 sm:mb-12 mt-6 md:mb-24 gap-y-6">
+                <div class="breadcrumbs text-xs md:text-sm subtitleTwo mb-0 text-white font-medium"><?php display_breadcrumb(); ?></div>
+                
+            </div>
+            
+        </div>
         <div class="row">
             <div class="col lg:col-span-8 lg:col-start-3">
                 <h1 class="headingTwo text-white text-center"><?php echo single_cat_title() ; ?></h1>
