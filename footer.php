1008ffb8dab91061efb68678c5a62253ba0f9663
David McDonagh <david@leapfrogdbs.co.uk>
2023-01-02 11:21:30 +0000

    Updates

--- footer.php
+++ footer.php
@@ -130,14 +130,14 @@ $cta_link = get_field('cta_link', 'option');
     <div class="newsletter">
 		<div class="bg-turquoise relative">
 			<div class="row items-center">
-				<div class="col col-span-12 lg:col-span-6 h-full">
+				<div class="col col-span-12 md::col-span-6 h-full">
 				<?php if($cta_image) { ?>
                 	<img class=" w-full h-full object-cover object-right" src="<?php echo $cta_image['url']; ?>" loading="lazy" alt="cta image">
 				<?php } else { ?>
 					<img class=" w-full h-full object-cover object-right" src="<?php echo get_template_directory_uri(); ?>/img/business.jpg" loading="lazy" alt="cta image">
 				<?php } ?>
 				</div>
-				<div class="col col-span-12 lg:col-span-5 px-10 lg:pr-0 py-10">
+				<div class="col col-span-12 md::col-span-5 px-10 md::pr-0 py-10">
 					<div id="post-content" class="entry-content entry-content block-editor-content white">
 					<?php echo $cta_copy; ?>
 					</div>	
